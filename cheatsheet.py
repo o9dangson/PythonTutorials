fb98ae1d6e0a9da2c53c3c7e14e820b4d76fa07e
Andrew Dang <o9dangson@gmail.com>
2020-05-08 16:56:15 -0400

    Updated Cheatsheet, added new material (Operators, Namespace) and Formatted for cleaner view

--- cheatsheet.py
+++ cheatsheet.py
@@ -5,7 +5,9 @@
                                                     #   Comments are useful for keeping organized, clean code.
 print("Hello World! This is the first commit!")     #   This is a print statement. It prints to the screen the string inside quotation marks.
 
+################################################################
 # Python Keywords (Must be type as they are seen)
+################################################################
 """ List of Keywords
     False       await   else    import  pass        None    
     break       except  in      raise   True        class
@@ -14,7 +16,9 @@ print("Hello World! This is the first commit!")     #   This is a print statemen
     while       assert  del     global  not         with
     async       elif    if      or      yield
 """
+################################################################
 # Python Identifier
+################################################################
 variable = 1                                        #   Identifiers can be any length, but cannot hold special characters and can't start with a number
 x3 = 2                                              #   Python is case sensitive
 my_Var_Y = 'This identifier is valid'               #   Underscore, '_', can be used in identifiers
@@ -22,7 +26,9 @@ myList = [1,2,3]
 # 3Var is not valid
 print("variable = %d; x3 = %d; my_Var_Y = %s; myList = %s" % (variable, x3, my_Var_Y, myList))   # %d refers to numbers, %s refers to strings or lists
 
+################################################################
 # Python Statement, Indentation, and comments
+################################################################
 a = 1 + 2 + \
     3                                               # Multilined statement
 b = (1 + 2 + 
@@ -44,7 +50,9 @@ import constant                                     # Constants are usually crea
 print(constant.PI)
 print(constant.GRAVITY)
 
+################################################################
 # Literals
+################################################################
 a = 0b1010      # Binary Literal:   10
 b = 100         # Decimal Literal:  100
 c = 0o310       # Octal Literal:    200
@@ -74,7 +82,9 @@ print("x is", x)
 food = None # Special Literal used to specify that the field has not been assigned
 print(food)
 
+################################################################
 # Literal Collections
+################################################################
 fruits = ["apple", "mango", "orange"]               #list
 numbers = (1, 2, 3)                                 #tuple
 alphabets = {'a': 'apple', 'b': 'ball', 'c': 'cat'} #dictionary
@@ -84,10 +94,101 @@ print(numbers)
 print(alphabets)
 print(vowels)
 
+################################################################
 # Python Datatypes
+################################################################
 a = 5
 print(a, "is of type", type(a))
 a = 2.0
 print(a, "is of type", type(a))
 a = 1+2j
 print(a, "is complex number:", isinstance(a, complex))
+
+#   [] Lists don't have to be the same type
+#   () Tuples can't be modified
+#   {} Sets are unordered collection of items
+#   {:} Dictionaries are unordered collection of key-value pairs
+
+#   Conversion btwn compatible types is possible 
+#       ie. int(), float(), str(), set(), tuple(), list(), dict()
+
+################################################################
+# Python Conversion
+################################################################
+#   implicit conversion is done automaticlly without user involvement
+#   explicit conversion is done by user
+num_int = 123
+num_str = "456"
+print("Data type of num_int:", type(num_int))
+print("Data type of num_str before Type Casting:", type(num_str))
+
+num_str = int(num_str)
+print("Data type of num_str after Type Casting:", type(num_str))
+
+num_sum = num_int + num_str
+print("Sum of num_int and num_str:",num_sum)
+print("Data type of the sum:", type(num_sum))
+
+################################################################
+# Python Input, Output and Import
+################################################################
+#   Output Formatting
+x = 5; y = 10
+print('The value of x is {} and y is {}'.format(x,y))
+print('The value of x is {1} and y is {0}'.format(x,y))     # Order can be designated in {}
+print('Hello {name}, {greeting}'.format(greeting = 'Good Morning!', name = 'John'))
+                # Keyword arguments can also be used
+print('The value of x is %3.2f' %x)         # Also can be done using old sprintf() in C
+           
+#   Input
+#           input(prompt)   is the syntax. Note that the value stored is a string datatype
+str_example = input(["Prompt for Input"])
+print('str_example:', str_example)
+
+################################################################
+# Python Operators
+################################################################
+# Arithmetic Operators
+#   + - * / 
+#   % (modulus)   // (Floor division)     ** (Exponent)
+# Comparator Operators
+#   > < == != =! >= <=
+# Logical Operators
+#   and   or  not
+# Bitwise Operators
+''' &   (bitwise AND)
+    |   (bitwise OR)
+    ~   (bitwise NOT)
+    ^   (bitwise XOR)
+    >>  (bitwise right shift)
+    <<  (bitwise left shift)
+'''
+# Assignment Operators (does calculation before assignment)
+#           =   +=  -=  *=  /=  **= &=  |=  ^=  >>= <<=
+#           eg. x+=5  is the same as x = x+5
+
+# Special Operators (is, is not)
+#    Identity Operators (used to check if two values/variables located in same memory)
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+
+print(x1 is not y1)         # False
+print(x2 is y2)             # True
+print(x3 is y3)             # False
+
+#   Membership Operators (in, not in)
+x = 'Hello World'
+y = {1:'a', 2:'b'}
+
+print('H' in x)         # True
+print('hello' not in x) # True
+print(1 in y)           # True
+print('a' in y)         # False; a is a value not a key so it isn't found
+
+################################################################
+# Python Namespace and Scope
+################################################################
